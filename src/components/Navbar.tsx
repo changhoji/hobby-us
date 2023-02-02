044887d3add60d8928c4ea577363a8104ddb8d80
changhoji <onkkno1@gmail.com>
2023-02-03 03:05:20 +0900

    Feat: signIn & signOut with google account

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import AuthButton from "./buttons/AuthButton";
 
 export default function Navbar() {
     return (
         <>
             <nav>
-                <Link href="/">Home</Link>
+                <Link href="/">[Title]</Link>
+                <Link href="/record">기록해요</Link>
                 <Link href="/my">MyPage</Link>
+                <AuthButton />
             </nav>
             <style jsx>{`
                 nav {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import Layout from "@/components/layouts/Layout";
 import type { AppProps } from "next/app";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
@@ -8,3 +10,15 @@ export default function App({ Component, pageProps }: AppProps) {
         </Layout>
     );
 }
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+export const fbApp = initializeApp(firebaseConfig);
+export const fbAuth = getAuth(fbApp);
